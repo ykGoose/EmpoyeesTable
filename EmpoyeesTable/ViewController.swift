fc0bed392b4b8fd73f7e52eb11db2261b24dd812
ykGoose <yokogus@gmail.com>
2020-11-04 17:11:11 +0900

    Initial Commit

---
+++ EmpoyeesTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmpoyeesTable
+//
+//  Created by Владимир Падусов on 04.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
